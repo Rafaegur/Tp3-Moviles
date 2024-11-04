c8dc4b6bcc4076c589ac1ed58b9487a3c59abd75
Rafael Eguren <rafaegur29@gmail.com>
2024-11-04 20:33:27 -0300

    Termino de configurar el Drawer y con el la navegación

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,15 @@
 import { Slot } from 'expo-router';
 import { Drawer } from 'expo-router/drawer'
+import { StatusBar } from 'expo-status-bar';
 
 const DrawerLayout = () => {
-  return <Drawer/>
+  return (
+    <Drawer>
+      <Drawer.Screen name="Home" options={{ title: 'Inicio' }} />
+      <Drawer.Screen name="AddTask" options={{ title: 'Agregar Tarea' }} />
+      <Drawer.Screen name="TaskDetails" options={{ title: 'Detalles de Tarea' }} />
+    </Drawer>
+  );  
 };
 
 export default DrawerLayout;
